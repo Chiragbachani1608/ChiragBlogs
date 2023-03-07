8729a46cedde244886df95b8efeb88566b953f3e
Chiragbachani1608 <107796393+Chiragbachani1608@users.noreply.github.com>
2023-03-07 23:58:10 +0530

    Create blogs.php

---
+++ blogs.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My Blog Post</title>
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <h1>My Blog Post</h1>
+    <p>Posted on January 1, 2023 by John Doe</p>
+    <hr>
+    <p>This is my first blog post. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit nunc sed metus accumsan, eu fermentum eros vehicula. Donec eu sagittis nisl. Sed nec nibh aliquam, consectetur mi eget, sollicitudin felis. Praesent in ligula vel lectus faucibus mollis. Sed posuere eros sit amet odio posuere, vel molestie magna eleifend. Donec ac nulla non lorem posuere euismod. Phasellus ullamcorper ligula vel leo euismod interdum. Vivamus placerat justo nec mi maximus consectetur. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse porttitor purus eu nibh fermentum faucibus.</p>
+    <p>Curabitur aliquam dolor in eros tempor, non hendrerit augue tincidunt. Maecenas eget velit et elit vulputate euismod. Etiam dapibus magna in nisi vehicula, at malesuada enim posuere. Proin pellentesque gravida est, ac malesuada lacus dapibus in. Aenean finibus sollicitudin ex, quis dapibus velit bibendum in. Nullam vel est sit amet leo ultricies egestas. Aliquam tempus nibh quis massa finibus, ac laoreet odio sagittis. Vivamus vehicula massa eget orci pretium, ac blandit nisl lobortis. Integer lobortis est id elit faucibus auctor. Nam accumsan augue eu mauris lobortis, vel tristique nibh dictum. Aliquam mollis ipsum sit amet nisi tempor, ut pharetra nisl tincidunt. Sed ac tellus magna. Donec fermentum ut augue a semper.</p>
+    <p>Donec vel enim sagittis, efficitur augue vel, pharetra mauris. In hac habitasse platea dictumst. Aliquam at magna et augue bibendum ornare. Sed tincidunt fringilla erat ac rutrum. Aliquam cursus magna ac lectus fermentum vehicula. Nam et neque a lectus feugiat tristique. Nulla facilisi. Nunc id justo in felis aliquam elementum. Donec auctor massa eu arcu mattis, euismod vestibulum dolor posuere. Ut euismod metus auctor, dictum orci vel, blandit lorem. Vivamus eu mauris mauris. Duis eget blandit ipsum. Donec pretium augue vel leo dictum, vitae eleifend tellus semper.</p>
+    <hr>
+    <p>Leave a comment:</p>
+    <form action="submit-comment.php" method="post">
+      <label for="name">Name:</label>
+      <input type="text" id="name" name="name"><br>
+      <label for="email">Email:</label>
+      <input type="email" id="email" name="email"><br>
+      <label for="comment
